cae9c23ed02a3db2417ef39f0053388f334f363d
Marcos Jesús Vivar <markich@Magi.local>
2014-02-15 18:36:00 -0300

    Added a controller to add a song to the playlist.

---
+++ iosclient/CollectiveMusicJB/SongAdditionViewController.h
@@ -0,0 +1,13 @@
+//
+//  SongAdditionViewController.h
+//  CollectiveMusicJB
+//
+//  Created by Marcos Jesús Vivar on 2/15/14.
+//  Copyright (c) 2014 Franco Agustín Rabaglia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SongAdditionViewController : UIViewController
+
+@end
