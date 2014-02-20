52e135400673516804f972f98d0e03b3a98b30a0
Marcos Jesús Vivar <markich@Magi.local>
2014-02-20 17:44:47 -0300

    Added a class for the app configuration constants.

---
+++ iosclient/CollectiveMusicJB/CollectiveJukeBoxAppConfiguration.h
@@ -0,0 +1,15 @@
+//
+//  CollectiveJukeBoxAppConfiguration.h
+//  CollectiveMusicJB
+//
+//  Created by Marcos Jesús Vivar on 2/15/14.
+//  Copyright (c) 2014 Franco Agustín Rabaglia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define API_SIGNATURE @"http://collectivemock.herokuapp.com/api/v1/"
+
+#define API_SEARCH @"search/"
+#define API_PLAYLIST_ADD @"room/playlist/add"
+#define API_ROOM @"room"
